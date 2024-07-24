f0c2d80458ccb9dd09d6632c3ae49898956c7b56
David <david.tomi2020@gmail.com>
2024-07-24 15:41:53 +0100

    created objects for weather icons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Weather } from "./weather";
 function App() {
   return (
     <div className="App">
+
       <Weather />
     </div>
   );
--- src/weather.jsx
+++ src/weather.jsx
@@ -1,11 +1,40 @@
-import { useState } from "react";
-import "./App.css";
+import clearIcon from "./Assets/clear.png";
+import cloudIcon from "./Assets/cloud.png";
+import drizzleIcon from "./Assets/drizzle.png";
+import thunderstormIcon from "./Assets/lightning.png";
+import humidityIcon from "./Assets/humidity.png";
+import rainIcon from "./Assets/rain.png";
+import snowIcon from "./Assets/snow.png";
+import windIcon from "./Assets/wind.png";
+
+import { useState, useEffect } from "react";
+import search from "./Assets/search.svg";
+import "./weather.css";
 export const Weather = () => {
   const apiKey = "2f7edf7443730c1e453a451e36e2ee93";
-  const [cityName, setCityName] = useState("");
+  const [cityName, setCityName] = useState("Sydney");
   const [data, setData] = useState(null);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const icons = {
+    "01d": clearIcon,
+    "01n": clearIcon,
+    "02d": cloudIcon,
+    "02n": cloudIcon,
+    "03d": cloudIcon,
+    "03n": cloudIcon,
+    "04d": drizzleIcon,
+    "04n": drizzleIcon,
+    "09d": rainIcon,
+    "09n": rainIcon,
+    "10d": rainIcon,
+    "10n": rainIcon,
+    "11n": thunderstormIcon,
+    "11d": thunderstormIcon,
+    "13d": snowIcon,
+    "13n": snowIcon,
+  };
   const fetchData = async () => {
     setLoading(true);
     setError(false);
@@ -15,37 +44,45 @@ export const Weather = () => {
       );
       if (!res.ok) throw Error("Could not fetch resource");
       const data = await res.json();
-      setData(data);
+      const icon = icons[data.weather[0].icon];
+      setData({ ...data, icon: icon });
       setLoading(false);
       return data;
     } catch (error) {
       setLoading(false);
-      setError(false);
       setError(error.message);
     }
   };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div>
-      <div>
-        {loading && <h1> Loading... </h1>}
-        {error && <h1>{error} </h1>}
+    <div className="weather">
+      {/* Weather Search Section */}
+      <div className="weather-search">
+        <input
+          type="text"
+          onChange={(e) => {
+            setCityName(e.target.value);
+          }}
+          placeholder="Search City..."
+        />
+        <button onClick={fetchData}>
+          <img src={search} alt="" />{" "}
+        </button>
       </div>
-      <input
-        type="text"
-        onChange={(e) => {
-          setCityName(e.target.value);
-        }}
-      />
-      <button onClick={fetchData}>Search</button>
-      {data &&
-        data.weather.map((weather) => (
-          <div key={weather.id}>
-            <h1>Weather:{weather.main}</h1>{" "}
-            <h2>Description: {weather.description}</h2>
-          </div>
-        ))}
-      <p>Lon:{data?.coord.lon} </p>
-      <p>Lat: {data?.coord.lat} </p>
+      {/* Loading and Error message */}
+      <div className="error-message">
+        {loading && <h2> Loading... </h2>}
+        {error && <h2>{error} </h2>}
+      </div>
+      {data && (
+        <div className="weather-info">
+          <img src={data?.icon} alt="icons" height={"200px"} />
+          <h1>{(data.main.temp - 273.15).toFixed(0)}⁰C </h1>
+{/* <h1>{data.} </h1> */}
+        </div>
+      )}
     </div>
   );
 };
